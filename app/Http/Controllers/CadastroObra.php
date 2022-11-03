73242386c5a1bcada3cc4a2485287bf0c6d348f6
adrianosolsa <116041006+adrianosolsa@users.noreply.github.com>
2022-11-02 23:23:15 -0300

    mudanças

--- app/Http/Controllers/CadastroObra.php
+++ app/Http/Controllers/CadastroObra.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class CadastroObra extends Controller
 {
     public function index() {
-        return view('site.cadastroobra');
+        return view('site.obras.index');
     }
 }
--- app/Http/Controllers/CadastroProduto.php
+++ app/Http/Controllers/CadastroProduto.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class CadastroProduto extends Controller
 {
     public function index() {
-        return view('site.cadastroproduto');
+        return view('site.produtos.index');
     }
 }
